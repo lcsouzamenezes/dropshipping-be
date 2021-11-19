dffecf2489b3ef99f5de8393224bf0dc4634d735
Jonathan Bertoldi <contato@jonathanbertoldi.com.br>
2021-11-19 16:13:49 -0300

    /me route with optional permissions

--- src/modules/users/services/GetSelfInformation/GetSelfInformationController.ts
+++ src/modules/users/services/GetSelfInformation/GetSelfInformationController.ts
@@ -1,14 +1,27 @@
 import { Request, Response } from 'express'
 import { container } from 'tsyringe'
+import { ObjectFlags } from 'typescript'
+import { GetSelfPermissionsService } from '../GetSelfPermissions/GetSelfPermissionsService'
 import { GetSelfInformationService } from './GetSelfInformationService'
 
 class GetSelfInformationController {
   async handle(request: Request, response: Response): Promise<Response> {
+    const { permissions } = request.query
+
     const getSelfInformationService = container.resolve(
       GetSelfInformationService
     )
 
     const selfInfo = await getSelfInformationService.execute(request.user.id)
+
+    if (permissions === 'true') {
+      const getSelfPermissions = container.resolve(GetSelfPermissionsService)
+      const permissions = await getSelfPermissions.execute(request.user.id)
+      Object.assign(selfInfo, {
+        ...permissions,
+      })
+    }
+
     return response.json({ me: selfInfo })
   }
 }

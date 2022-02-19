862207a6ac43fa8b11d93fb5bfbc3ccd51f74b24
Jonathan Bertoldi <contato@jonathanbertoldi.com.br>
2022-02-19 10:26:26 -0300

    add user to account in auth reqs

--- src/modules/accounts/mappers/AccountsMapper.ts
+++ src/modules/accounts/mappers/AccountsMapper.ts
@@ -1,13 +1,19 @@
+import { UserMapper } from '@modules/users/mappers/UserMapper'
 import { AccountDTO } from '../dtos/AccountDTO'
 import { Account } from '../infra/typeorm/entities/Account'
 
 class AccountsMapper {
-  static toDTO({ id, name, type }: Account): AccountDTO {
+  static toDTO({ id, name, type, user }: Account): AccountDTO {
     const account = {
       id,
       name,
       type,
     } as AccountDTO
+
+    if (user) {
+      const userDTO = UserMapper.toDTO(user)
+      Object.assign(account, { user: userDTO })
+    }
 
     return account
   }

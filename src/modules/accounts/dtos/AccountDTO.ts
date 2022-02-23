30dbd2b5e3bc121f65180e699cde867e7f95d86d
Jonathan Bertoldi <contato@jonathanbertoldi.com.br>
2022-02-22 22:28:00 -0300

    add addess to suppliers

--- src/modules/accounts/dtos/AccountDTO.ts
+++ src/modules/accounts/dtos/AccountDTO.ts
@@ -1,7 +1,10 @@
+import { AddressDTO } from '@modules/addresses/mappers/AddressMapper'
+
 interface AccountDTO {
   id: string
   name: string
   type: string
+  address?: AddressDTO
 }
 
 export { AccountDTO }
--- src/modules/accounts/mappers/AccountsMapper.ts
+++ src/modules/accounts/mappers/AccountsMapper.ts
@@ -1,10 +1,23 @@
+import { Address } from '@modules/addresses/infra/typeorm/entities/Address'
+import { AddressMapper } from '@modules/addresses/mappers/AddressMapper'
 import { ProfileMapper } from '@modules/profiles/mappers/ProfileMapper'
 import { UserMapper } from '@modules/users/mappers/UserMapper'
 import { AccountDTO } from '../dtos/AccountDTO'
 import { Account } from '../infra/typeorm/entities/Account'
 
+interface AccountMapperData extends Account {
+  address?: Address
+}
+
 class AccountsMapper {
-  static toDTO({ id, name, type, profile, user }: Account): AccountDTO {
+  static toDTO({
+    id,
+    name,
+    type,
+    profile,
+    user,
+    address,
+  }: AccountMapperData): AccountDTO {
     const account = {
       id,
       name,
@@ -14,6 +27,11 @@ class AccountsMapper {
     if (profile) {
       const profileDTO = ProfileMapper.toDTO(profile)
       Object.assign(account, { profile: profileDTO })
+    }
+
+    if (address) {
+      const addressDTO = AddressMapper.toDTO(address)
+      Object.assign(account, { address: addressDTO })
     }
 
     if (user) {
